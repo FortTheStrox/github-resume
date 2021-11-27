7e4fc4c428e820554618265848ce052f4dc03e06
Jason <jarederl@protonmail.ch>
2021-11-27 14:06:01 -0600

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+// $( document ).ready(function() {
+//     $('.main').load("main.html");
+// });
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    $( '.main' ).load('main.html');
+    console.log( "ready!" );
+});
