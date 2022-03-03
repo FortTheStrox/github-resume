ca41d57bcdfad9bd49a4258f0ce50f91308372dd
Jared E <jarederl@protonmail.ch>
2022-03-02 19:49:02 -0600

    added animate

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,8 @@
 
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    $( '.main' ).load('main.html');
-    console.log( "ready!" );
+    $( '.main' ).load('main.html', function() {
+        $("footer").delay(850).animate({"opacity": "1"},700);
+    });
+    $("#background").delay(200).animate({"opacity": "1"}, 700);
 });
