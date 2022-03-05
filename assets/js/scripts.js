b1caf70de442c69fdb959f6c8310f38dfb878090
Jared E <jarederl@protonmail.ch>
2022-03-05 11:19:15 -0600

    made about page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,7 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    $( '.nav' ).load('nav.html', function() {
-        // we specify id of fadein which is child of main class
-        $(".nav #fadein").delay(900).animate({"opacity": "1"},700);
-        
-    });
-    $( '.footer' ).load('footer.html', function() {
-        // we specify id of fadein which is child of main class
-        $("footer #fadein").delay(900).animate({"opacity": "1"},700); 
-    });
+    $( '.nav' ).load('nav.html');
+    $( '.footer' ).load('footer.html');
     // set from invisible to visible for each page's content
     $(".landing").delay(200).animate({"opacity": "1"},6100);
     $(".about").delay(200).animate({"opacity": "1"},700);
