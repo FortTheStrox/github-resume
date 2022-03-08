c6dd25e055456c26afd4d73fce33f1b818fa588f
Jared E <jarederl@protonmail.ch>
2022-03-07 19:35:14 -0600

    changed landing page fade in time

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     $( '.nav' ).load('nav.html');
     $( '.footer' ).load('footer.html');
     // set from invisible to visible for each page's content
-    $(".landing").delay(200).animate({"opacity": "1"},6100);
+    $(".landing").delay(200).animate({"opacity": "1"},1500);
     $(".about").delay(200).animate({"opacity": "1"},700);
     $(".skillset").delay(200).animate({"opacity": "1"},700);
     $(".resume").delay(200).animate({"opacity": "1"},700);
