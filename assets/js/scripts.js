e16b08fc9855653aa9856875de533641611f3453
Jared E <jarederl@protonmail.ch>
2022-03-09 21:25:23 -0600

    added button filter logic

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,13 +8,75 @@ $( document ).ready(function() {
     $(".skillset").delay(200).animate({"opacity": "1"},700);
     $(".resume").delay(200).animate({"opacity": "1"},700);
     
+    // TOGGLE BUTTON LOGIC
+    $('.category_all').on('click', function() {
+        const elements = document.querySelectorAll('.card');
+        Array.from(elements).forEach((element) => {
+            // KEYFRAME ANIMATION FOR FADE IN
+            element.animate([
+                {opacity: 0},
+                {opacity: 1}]
+                , 800);
+            element.style.display = "flex";
+        })
+    });
     $('.category1').on('click', function() {
-        $('.card1').slideToggle(300);
+        let filter = 'card1';
+        const elements = document.querySelectorAll('.card');
+        Array.from(elements).forEach((element) => {
+            if(element.className.includes(filter)) {
+                // KEYFRAME ANIMATION FOR FADE IN
+                element.animate([
+                    {opacity: 0},
+                    {opacity: 1}]
+                    , 800);
+                element.style.display = "flex";
+            } else {
+                // KEYFRAME ANIMATION FOR FADE OUT
+                element.animate([
+                    {opacity: 1},
+                    {opacity: 0}]
+                    , 800);
+                element.style.display = "none";
+            }
+        })
     });
     $('.category2').on('click', function() {
-        $('.card2').slideToggle(300);
+        let filter = 'card2';
+        const elements = document.querySelectorAll('.card');
+        Array.from(elements).forEach((element) => {
+            if(element.className.includes(filter)) {
+                element.animate([
+                    {opacity: 0},
+                    {opacity: 1}]
+                    , 800);
+                element.style.display = "flex";
+            } else {
+                element.animate([
+                    {opacity: 1},
+                    {opacity: 0}]
+                    , 800);
+                element.style.display = "none";
+            }
+        })
     });
     $('.category3').on('click', function() {
-        $('.card3').slideToggle(300);
+        let filter = 'card3';
+        const elements = document.querySelectorAll('.card');
+        Array.from(elements).forEach((element) => {
+            if(element.className.includes(filter)) {
+                element.animate([
+                    {opacity: 0},
+                    {opacity: 1}]
+                    , 800);
+                element.style.display = "flex";
+            } else {
+                element.animate([
+                    {opacity: 1},
+                    {opacity: 0}]
+                    , 800);
+                element.style.display = "none";
+            }
+        })
     });
 });
