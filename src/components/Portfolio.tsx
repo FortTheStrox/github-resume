abfffb5536c30008c4ee1b819a1c23ce45968705
Jared E <jarederl@protonmail.ch>
2022-06-18 20:57:50 -0500

    created foundation for portfolio filter

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,113 +1,104 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import { ToggleButton, ToggleButtonGroup } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 
 import ceTest from './../img/portfolio/ce.png';
 
 import './../css/main.min.css';
-import { ModuleReference } from 'typescript';
+
 
 type Props = {
-  works: {
-    id: number, 
-    title: string, 
-    tags: string[], 
-    desc: string, 
-    class: string, 
-  }[]
-  hobby: {
-    id: number, 
-    title: string, 
-    tags: string[], 
-    desc: string, 
-    img: ModuleReference, 
-    class: string, 
-    path: string
-  }[]
-}
-
-type state = {
   //
 }
 
-
-function handleChange() {
-
+type state = {
+  filter : string
 }
 
-export default class Portfolio extends React.Component<Props,state> {
-  constructor(props: Props) {
-    super(props);
 
-    props.works = [
-      {
-        id: 0,
-        title: 'User Interface for Undisclosed Product',
-        tags: ['Angular'],
-        desc: 'Providing a intuitive user interface for team\'s main product',
-        class: 'tes',
-      }
-    ]
-    // props.hobby = [
-    //   {
-    //     'id': 1,
-    //     'title': 'External Cheat Trainer',
-    //     'tags': ['Cheat Engine', 'Visual Code', 'C++'],
-    //     'desc': 'Providing a easy to use trainer with provided keybinds',
-    //     'img': ceTest,
-    //     'class': 'hobTest',
-    //     'path': 'placeholder'
-    //   }
-    // ]
-  };
-  render() {
-    return (
-      <div className='Portfolio'>
-        <div className='title'>Software Engineer, Security Analyst, Adrenaline Seeker</div>
-        <div className='intro'>
-          <p>Hello! I'm a software engineer who likes security.
-            <br />
-            I'm currently working on protecting systems
-            <br />
-            at an undisclosed company!
 
-          </p>
-        </div>
-        <div className='body'>
-          <p>filter:</p>
-        <ToggleButtonGroup 
-          exclusive
-          onChange={handleChange}
-        >
-          <ToggleButton value='work'>
-            Work
-          </ToggleButton>
-          <ToggleButton value='hobby'>
-            Hobby
-          </ToggleButton>
-        </ToggleButtonGroup>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia>
-
-            </CardMedia>
-            <CardContent>
-
-            </CardContent>
-            <Typography>
-              test
-            </Typography>
-          </CardActionArea>
-
-        </Card>
-
-        </div>
-      </div>
-    );
+// Define Card attributes here 
+const works = [
+  {
+    id: 0,
+    title: 'User Interface for Undisclosed Product',
+    tags: ['Angular'],
+    desc: 'Providing a intuitive user interface for team\'s main product',
+    class: 'tes',
+  },
+  {
+    id: 2,
+    title: 'User Interface for Undisclosed Product',
+    tags: ['Angular'],
+    desc: 'Providing a intuitive user interface for team\'s main product',
+    class: 'tes',
   }
-    
+]
+
+const hobbies = [
+  {
+    'id': 1,
+    'title': 'External Cheat Trainer',
+    'tags': ['Cheat Engine', 'Visual Code', 'C++'],
+    'desc': 'Providing a easy to use trainer with provided keybinds',
+    'img': ceTest,
+    'class': 'hobTest',
+    'path': 'placeholder'
+  }
+]
+
+const Portfolio = () => {
+  const [filter, setFilter] = React.useState('work');
+
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newFilter: string,
+    ) => {
+      console.log(filter);
+      setFilter(newFilter);
+      
+    }
+  
+
+  return (
+    <div className='Portfolio'>
+      <div className='title'>Software Engineer, Security Analyst, Adrenaline Seeker</div>
+      <div className='intro'>
+        <p>Hello! I'm a software engineer who likes security.
+          <br />
+          I'm currently working on protecting systems
+          <br />
+          at an undisclosed company!
+
+        </p>
+      </div>
+
+      {/* Section for Card Filter Toggle */}
+      <div className='body'>
+        <p>filter:</p>
+      <ToggleButtonGroup 
+        value={filter}
+        exclusive
+        onChange={handleChange}
+      >
+        <ToggleButton value='work'>
+          Work
+        </ToggleButton>
+        <ToggleButton value='hobby'>
+          Hobby
+        </ToggleButton>
+      </ToggleButtonGroup>
+
+      {/* Cards */}
+      
+      </div>
+    </div>
+  );
 }
+
+
+export default Portfolio;
---
+++ src/components/Project.tsx
@@ -0,0 +1,27 @@
+// import React from 'react';
+
+// type Prop = {
+//     //
+// }
+
+// const Project = ({ item }) => {
+//     return (
+//         <Card 
+//         sx={{ maxWidth: 345 }} 
+//         key={item.works[0].id}
+//         >
+//         <CardActionArea>
+//         <CardContent>
+//             <Typography gutterBottom variant="h5" component="div">
+//                 {works[0].title}
+//             </Typography>
+//             <Typography variant="body2" color="text.secondary">
+//             {works[0].desc}
+//             </Typography>
+//             </CardContent>
+//             </CardActionArea>
+//         </Card>  
+//     )
+// }
+
+export default {};
